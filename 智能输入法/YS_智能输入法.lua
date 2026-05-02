463aeec304d145a5cbd1c7b21e51b8b530eda1ed
YS-YYDS <782945305@qq.com>
2026-05-02 21:17:04 +0800

    Release 1 V1.2.2 (Silent Fix)

--- 智能输入法/YS_智能输入法.lua
+++ 智能输入法/YS_智能输入法.lua
@@ -4,4 +4,4 @@
 -- @build 2026-04-30 08:08:16
 -- @ys_auth_id 1
 
-local function _L() local i=debug.getinfo(1,'S') local p=(i.source:sub(1,1)=='@') and i.source:sub(2) or '' local d=p:match('^(.*[\\/])') or '' reaper.SetExtState("YS_Runtime","RootDir",d,true) local dat_path=d..'YS_智能输入法.dat' local hf=io.open(dat_path,'rb') if not hf then reaper.MB('无法打开: '..dat_path,'加载错误',0) return end local c=hf:read('*all') hf:close() local f,e=load(c) if f then f() else reaper.MB('无法加载: '..dat_path..'\n错误: '..tostring(e),'加载错误',0) end end _L()
+local function _L() local i=debug.getinfo(1,'S') local p=(i.source:sub(1,1)=='@') and i.source:sub(2) or '' local d=p:match('^(.*[\\/])') or '' reaper.SetExtState("YS_Runtime","RootDir",d,true) if reaper.file_exists(d.."Auth/YS_Auth.lua") then reaper.SetExtState("YS_Hub","InstallPath",d,true) end local dat_path=d..'YS_智能输入法.dat' local hf=io.open(dat_path,'rb') if not hf then reaper.MB('无法打开: '..dat_path,'加载错误',0) return end local c=hf:read('*all') hf:close() local f,e=load(c) if f then f() else reaper.MB('无法加载: '..dat_path..'\n错误: '..tostring(e),'加载错误',0) end end _L()
