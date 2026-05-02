aa5fc22c2428a4ecf118f60d8b79094b29d0c03d
YS-YYDS <782945305@qq.com>
2026-05-02 14:42:12 +0800

    Release 0 V0.7.0

--- YS_脚本管理器/YS_脚本管理器.lua
+++ YS_脚本管理器/YS_脚本管理器.lua
@@ -4,4 +4,4 @@
 -- @build 2026-05-02 01:04:09
 -- @ys_auth_id 0
 
-local function _L() local i=debug.getinfo(1,'S') local p=(i.source:sub(1,1)=='@') and i.source:sub(2) or '' local d=p:match('^(.*[\\/])') or '' reaper.SetExtState("YS_Runtime","RootDir",d,true) reaper.SetExtState("YS_Hub","InstallPath",d,true) local dat_path=d..'YS_脚本管理器.dat' local hf=io.open(dat_path,'rb') if not hf then reaper.MB('无法打开: '..dat_path,'加载错误',0) return end local c=hf:read('*all') hf:close() local f,e=load(c) if f then f() else reaper.MB('无法加载: '..dat_path..'\n错误: '..tostring(e),'加载错误',0) end end _L()
+local function _L() local i=debug.getinfo(1,'S') local p=(i.source:sub(1,1)=='@') and i.source:sub(2) or '' local d=p:match('^(.*[\\/])') or '' reaper.SetExtState("YS_Runtime","RootDir",d,true) local dat_path=d..'YS_脚本管理器.dat' local hf=io.open(dat_path,'rb') if not hf then reaper.MB('无法打开: '..dat_path,'加载错误',0) return end local c=hf:read('*all') hf:close() local f,e=load(c) if f then f() else reaper.MB('无法加载: '..dat_path..'\n错误: '..tostring(e),'加载错误',0) end end _L()
