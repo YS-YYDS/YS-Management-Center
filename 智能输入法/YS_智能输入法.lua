37ffb4a03adb195656bd1047dd4781a2288caa48
YS-YYDS <782945305@qq.com>
2026-05-15 21:23:51 +0800

    Release 1 V1.3.1 (Silent Fix)

--- 智能输入法/YS_智能输入法.lua
+++ 智能输入法/YS_智能输入法.lua
@@ -1,7 +1,7 @@
 -- @description 输入法智能切换，避免中文环境下快捷键冲突，支持自定义窗口规则，可视化控制面板，为创作流提供零干扰输入体验。
 -- @version 1.3.1
 -- @author YS / Antigravity
--- @build 2026-05-14 01:57:34
+-- @build 2026-05-15 21:21:09
 -- @ys_auth_id 1
 
 local function _L() local i=debug.getinfo(1,'S') local p=(i.source:sub(1,1)=='@') and i.source:sub(2) or '' local d=p:match('^(.*[\\/])') or '' reaper.SetExtState("YS_Runtime","RootDir",d,true) package.path=d..'?.lua;'..d..'Modules/?.lua;'..package.path local dat_path=d..'YS_智能输入法.dat' local hf=io.open(dat_path,'rb') if not hf then reaper.MB('无法打开: '..dat_path,'加载错误',0) return end local c=hf:read('*all') hf:close() local f,e=load(c) if f then f() else reaper.MB('无法加载: '..dat_path..'\n错误: '..tostring(e),'加载错误',0) end end _L()
