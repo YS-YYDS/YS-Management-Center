2c614a76a0d49f41e4bd1776398b9bb3592e311f
YS-YYDS <782945305@qq.com>
2026-05-20 13:54:38 +0800

    Release 0 V0.8.0

--- YS_脚本管理器/Modules/YS_Auth.lua
+++ YS_脚本管理器/Modules/YS_Auth.lua
@@ -1,7 +1,7 @@
 -- @description YS 脚本生态管理中心，支持插件安装、更新与授权管理。
 -- @version 0.8.0
 -- @author YS
--- @build 2026-05-18 15:34:19
+-- @build 2026-05-19 18:38:35
 -- @ys_auth_id 0
 
 local function _L() local i=debug.getinfo(1,'S') local p=(i.source:sub(1,1)=='@') and i.source:sub(2) or '' local d=p:match('^(.*[\\/])') or '' reaper.SetExtState("YS_Runtime","RootDir",d,true) package.path=d..'?.lua;'..d..'Modules/?.lua;'..package.path local dat_path=d..'YS_Auth.dat' local hf=io.open(dat_path,'rb') if not hf then local up=d:match('^(.-)[^/\\]+[/\\]$') or '' if up~=d and up~='' then hf=io.open(up..'YS_Auth.dat','rb') end end if not hf then return nil end local c=hf:read('*all') hf:close() local f,e=load(c) if f then return f() end return nil end return _L()
